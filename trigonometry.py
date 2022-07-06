fe9df8e9322902724d41f0beb5b0e09d7e8c61fb
VulcanWM <logmedha09@gmail.com>
2022-07-06 21:42:54 +0100

    added 6 more trigonometry functions in trigonometry.py

--- trigonometry.py
+++ trigonometry.py
@@ -20,3 +20,33 @@ def get_angle_from_adj_hyp(a, h):
     radian = math.acos(num)
     degrees = math.degrees(radian)
     return degrees
+
+
+def get_adj_from_angle_hyp(angle, h):
+    a = h * math.cos(math.radians(angle))
+    return a
+
+
+def get_opp_from_angle_hyp(angle, h):
+    o = h * math.sin(math.radians(angle))
+    return o
+
+
+def get_adj_from_angle_opp(angle, o):
+    a = o / math.tan(math.radians(angle))
+    return a
+
+
+def get_opp_from_angle_adj(angle, a):
+    o = a * math.tan(math.radians(angle))
+    return o
+
+
+def get_hyp_from_angle_adj(angle, a):
+    h = a / math.cos(math.radians(angle))
+    return h
+
+
+def get_hyp_from_angle_opp(angle, o):
+    h = o / math.sin(math.radians(angle))
+    return h
