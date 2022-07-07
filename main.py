645497de6195ebddbd1d02f996d625417303fab6
VulcanWM <logmedha09@gmail.com>
2022-07-07 20:15:32 +0100

    added rounding in the functions

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import trigonometry
 
 
-print(trigonometry.get_angle_from_opp_hyp(6, 13))
-print(trigonometry.get_angle_from_opp_adj(6, 5))
-print(trigonometry.get_angle_from_adj_hyp(6, 13))
+print(trigonometry.get_angle_from_opp_adj(4, 5, 2))
--- trigonometry.py
+++ trigonometry.py
@@ -1,52 +1,52 @@
 import math
 
 
-def get_angle_from_opp_hyp(o, h):
+def get_angle_from_opp_hyp(o, h, dec=2):
     num = o / h
     radian = math.asin(num)
     degrees = math.degrees(radian)
-    return degrees
+    return round(degrees, dec)
 
 
-def get_angle_from_opp_adj(o, a):
+def get_angle_from_opp_adj(o, a, dec=2):
     num = o / a
     radian = math.atan(num)
     degrees = math.degrees(radian)
-    return degrees
+    return round(degrees, dec)
 
 
-def get_angle_from_adj_hyp(a, h):
+def get_angle_from_adj_hyp(a, h, dec=2):
     num = a / h
     radian = math.acos(num)
     degrees = math.degrees(radian)
-    return degrees
+    return round(degrees, dec)
 
 
-def get_adj_from_angle_hyp(angle, h):
+def get_adj_from_angle_hyp(angle, h, dec=2):
     a = h * math.cos(math.radians(angle))
-    return a
+    return round(a, dec)
 
 
-def get_opp_from_angle_hyp(angle, h):
-    o = h * math.sin(math.radians(angle))
-    return o
-
-
-def get_adj_from_angle_opp(angle, o):
-    a = o / math.tan(math.radians(angle))
-    return a
-
-
-def get_opp_from_angle_adj(angle, a):
-    o = a * math.tan(math.radians(angle))
-    return o
-
-
-def get_hyp_from_angle_adj(angle, a):
+def get_hyp_from_angle_adj(angle, a, dec=2):
     h = a / math.cos(math.radians(angle))
-    return h
+    return round(h, dec)
 
 
-def get_hyp_from_angle_opp(angle, o):
+def get_opp_from_angle_hyp(angle, h, dec=2):
+    o = h * math.sin(math.radians(angle))
+    return round(o, dec)
+
+
+def get_hyp_from_angle_opp(angle, o, dec=2):
     h = o / math.sin(math.radians(angle))
-    return h
+    return round(h, dec)
+
+
+def get_adj_from_angle_opp(angle, o, dec=2):
+    a = o / math.tan(math.radians(angle))
+    return round(a, dec)
+
+
+def get_opp_from_angle_adj(angle, a, dec=2):
+    o = a * math.tan(math.radians(angle))
+    return round(o, dec)
