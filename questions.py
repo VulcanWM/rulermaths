ac87f15c441c5fc28937c4ff9d0a9e347f617c17
VulcanWM <logmedha09@gmail.com>
2022-07-14 20:54:08 +0100

    added an lcm question

--- questions.py
+++ questions.py
@@ -167,17 +167,21 @@ def prime_factorisation_question():
 
 
 def lowest_common_multiple_questions():
-    question_number = random.randint(1,2)
+    question_number = random.randint(1, 1)
     if question_number == 1:
-        number1 = random.randint(3,11)
-        number2 = random.randint(3,11)
+        number1 = random.randint(3, 11)
+        number2 = random.randint(3, 11)
         while number2 == number1:
             number2 = random.randint(3, 11)
         colour1 = random.choice(lists.colours)
         colour2 = random.choice(lists.colours)
         while colour2 == colour1:
             colour2 = random.choice(lists.colours)
-        question = f"A child is arranging green blocks in groups of {str(number1)}" \
-                   f" and purple blocks in groups of {str(number2)}. " \
-                   "If she has the same number of green and purple blocks, " \
-                   "what is the smallest number of each colour that she could have?"
+        question = f"A child is arranging {colour1} blocks in groups of {str(number1)}" \
+                   f" and {colour2} blocks in groups of {str(number2)}. " \
+                   f"If she has the same number of {colour1} and {colour2} blocks, " \
+                   f"what is the smallest number of each colour that she could have?"
+        answer = maths.lowest_common_multiple_2(number1, number2)
+        return question, answer
+
+print(lowest_common_multiple_questions())
