5833c560e7c8493fefa26c21f805a8787720f1a1
VulcanWM <logmedha09@gmail.com>
2022-07-15 21:19:26 +0100

    added hcf and lcm questions to the questions dict

--- sort_questions.py
+++ sort_questions.py
@@ -3,30 +3,34 @@ import questions
 all_questions = {
     "KS2": {
         "Number": [
-                questions.linear_sequence_question(1),
-                questions.linear_sequence_question(2),
-                questions.linear_sequence_question(3),
-                questions.is_prime_question(),
-                questions.prime_factorisation_question()
+            questions.linear_sequence_question(1),
+            questions.linear_sequence_question(2),
+            questions.linear_sequence_question(3),
+            questions.is_prime_question(),
+            questions.prime_factorisation_question(),
+            questions.lowest_common_multiple_questions(),
+            questions.highest_common_factor_question()
         ]
     },
     "KS3": {
-            "Number": [
-                questions.linear_sequence_question(1),
-                questions.linear_sequence_question(2),
-                questions.linear_sequence_question(3),
-                questions.is_prime_question(),
-                questions.prime_factorisation_question()
-            ],
-            "Trigonometry": [
-                questions.side_from_angle_side_question()
-            ],
-            "Fractions, Decimals and Percentages": [
-                questions.recurring_decimal_to_fraction_question(1),
-                questions.recurring_decimal_to_fraction_question(2),
-            ],
-            "Algebra": [
-                questions.x_on_both_sides_question()
-            ]
+        "Number": [
+            questions.linear_sequence_question(1),
+            questions.linear_sequence_question(2),
+            questions.linear_sequence_question(3),
+            questions.is_prime_question(),
+            questions.prime_factorisation_question(),
+            questions.lowest_common_multiple_questions(),
+            questions.highest_common_factor_question()
+        ],
+        "Trigonometry": [
+            questions.side_from_angle_side_question()
+        ],
+        "Fractions, Decimals and Percentages": [
+            questions.recurring_decimal_to_fraction_question(1),
+            questions.recurring_decimal_to_fraction_question(2),
+        ],
+        "Algebra": [
+            questions.x_on_both_sides_question()
+        ]
     }
 }
