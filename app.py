1b79ee2c256596b191170d71af3a59c0a37149bc
VulcanWM <logmedha09@gmail.com>
2022-07-19 20:13:05 +0100

    added a checkbox input form

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
+from sort_questions import all_questions
 
 app = Flask(__name__)
 
@@ -6,3 +7,22 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/form_page")
+def form_page():
+    return render_template("form.html", all_questions=all_questions)
+
+
+@app.route("/form", methods=['GET', 'POST'])
+def form_process():
+    if request.method == 'POST':
+        all_checkboxes = all_questions['KS3'].keys()
+        test_topics = []
+        for checkbox in all_checkboxes:
+            if request.form.get(checkbox):
+                test_topics.append(checkbox)
+        return ", ".join(test_topics)
+    else:
+        return redirect("/form_page")
+
