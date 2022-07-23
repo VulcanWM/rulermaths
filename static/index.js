53ca748dcdbe58acc0e316fe9c901e27c888c215
VulcanWM <logmedha09@gmail.com>
2022-07-23 19:45:16 +0100

    added an `index.js` file in `static` folder added google analytics

---
+++ static/index.js
@@ -0,0 +1,10 @@
+var Script1 = document.createElement("script");
+Script1.setAttribute("async", null);
+Script1.src =
+"https://www.googletagmanager.com/gtag/js?id=G-LN55VG0P9K";
+document.head.appendChild(Script1);
+
+var Script2 = document.createElement("script");
+Script2.innerText =
+"window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-LN55VG0P9K');";
+document.head.appendChild(Script2);
