ee68a1f6a6a0c5849e7affb2f0054a812db3a022
VulcanWM <logmedha09@gmail.com>
2022-07-23 19:25:58 +0100

    #12

--- sort_questions.py
+++ sort_questions.py
@@ -2,37 +2,63 @@ import questions
 
 all_questions = {
     "KS2": {
-        "Number": [
-            questions.linear_sequence_question(1),
-            questions.linear_sequence_question(2),
-            questions.linear_sequence_question(3),
-            questions.is_prime_question(),
-            questions.prime_factorisation_question(),
-            questions.lowest_common_multiple_questions(),
-            questions.highest_common_factor_question(),
-            questions.compare_numbers_question()
-        ]
+        "Number": {
+            "Linear Sequences": [
+                questions.linear_sequence_question(1),
+                questions.linear_sequence_question(2),
+                questions.linear_sequence_question(3)
+            ],
+            "Prime Numbers": [
+                questions.is_prime_question(),
+                questions.prime_factorisation_question()
+            ],
+            "LCM": [
+                questions.lowest_common_multiple_questions()
+            ],
+            "HCF": [
+                questions.highest_common_factor_question()
+            ],
+            "Compare numbers": [
+                questions.compare_numbers_question()
+            ]
+        }
     },
     "KS3": {
-        "Number": [
-            questions.linear_sequence_question(1),
-            questions.linear_sequence_question(2),
-            questions.linear_sequence_question(3),
-            questions.is_prime_question(),
-            questions.prime_factorisation_question(),
-            questions.lowest_common_multiple_questions(),
-            questions.highest_common_factor_question(),
-            questions.compare_numbers_question()
-        ],
-        "Trigonometry": [
-            questions.side_from_angle_side_question()
-        ],
-        "Fractions, Decimals and Percentages": [
-            questions.recurring_decimal_to_fraction_question(1),
-            questions.recurring_decimal_to_fraction_question(2),
-        ],
-        "Algebra": [
-            questions.x_on_both_sides_question()
-        ]
+        "Number": {
+            "Linear Sequences": [
+                questions.linear_sequence_question(1),
+                questions.linear_sequence_question(2),
+                questions.linear_sequence_question(3)
+            ],
+            "Prime Numbers": [
+                questions.is_prime_question(),
+                questions.prime_factorisation_question()
+            ],
+            "LCM": [
+                questions.lowest_common_multiple_questions()
+            ],
+            "HCF": [
+                questions.highest_common_factor_question()
+            ],
+            "Compare Numbers": [
+                questions.compare_numbers_question()
+            ]
+        },
+        "Trigonometry": {
+            "Side from Side and Angle": [
+                questions.side_from_angle_side_question()
+            ]
+        },
+        "Fractions, Decimals and Percentages": {
+            "Recurring Decimals": [
+                questions.recurring_decimal_to_fraction_question(1),
+                questions.recurring_decimal_to_fraction_question(2)
+            ]
+        },
+        "Algebra": {
+            "Solve linear equations": [
+                questions.x_on_both_sides_question()
+            ]
+        }
     }
 }
